a2eb6a67e77e484e2dee7ac1e8f01cfb6080e378
Laurent Juma <lcbillax.lj@gmail.com>
2019-10-16 10:04:33 +0300

    Set up Hero Class

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,64 @@
 package models;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Hero {
+    private String name;
+    private int id;
+    private int squadId;
+    private LocalDateTime createdAt;
+
+    public Hero(String name,int squadId){
+        this.name=name;
+        this.squadId=squadId;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSquadId() {
+        return squadId;
+    }
+
+    public void setSquadId(int squadId) {
+        this.squadId = squadId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hero hero = (Hero) o;
+        return id == hero.id &&
+                squadId == hero.squadId &&
+                Objects.equals(name, hero.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id, squadId);
+    }
 }
