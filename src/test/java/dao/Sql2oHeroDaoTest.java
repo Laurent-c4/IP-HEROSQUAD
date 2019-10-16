bf8b8423eb7a1275b9fb0c07d916517e0e8b27d5
Laurent Juma <lcbillax.lj@gmail.com>
2019-10-16 10:53:35 +0300

    Add tests for HeroDao

--- src/test/java/dao/Sql2oHeroDaoTest.java
+++ src/test/java/dao/Sql2oHeroDaoTest.java
@@ -1,17 +1,104 @@
+
 package dao;
 
-import org.junit.After;
-import org.junit.Before;
-
+import models.Hero;
+import org.sql2o.*;
+import org.junit.*;
 import static org.junit.Assert.*;
 
 public class Sql2oHeroDaoTest {
+    private static Sql2oHeroDao heroDao;
+    private static Connection conn;
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeClass
+    public static void setUp() throws Exception {
+        String connectionString = "jdbc:postgresql://localhost:5432/herosquad_test";
+        Sql2o sql2o = new Sql2o(connectionString, "laurent", "laurent");
+        heroDao = new Sql2oHeroDao(sql2o);
+        conn = sql2o.open();
     }
 
     @After
     public void tearDown() throws Exception {
+        System.out.println("clearing database");
+        heroDao.clearAllHeroes();
+
+    }
+
+    @AfterClass
+    public static void shutDown() throws Exception{
+        conn.close();
+        System.out.println("connection closed");
+    }
+
+    @Test
+    public void addingHeroSetsId() throws Exception {
+        Hero hero = setupNewHero();
+        int originalHeroId = hero.getId();
+        heroDao.add(hero);
+        assertNotEquals(originalHeroId, hero.getId());
+    }
+
+    @Test
+    public void existingHeroesCanBeFoundById() throws Exception {
+        Hero hero = setupNewHero();
+        heroDao.add(hero);
+        Hero foundHero = heroDao.findById(hero.getId());
+        assertEquals(hero, foundHero);
+    }
+
+    @Test
+    public void addedHeroesAreReturnedFromgetAll() throws Exception {
+        Hero hero = setupNewHero();
+        heroDao.add(hero);
+        assertEquals(1, heroDao.getAll().size());
+    }
+
+    @Test
+    public void noTasksReturnsEmptyList() throws Exception {
+        assertEquals(0, heroDao.getAll().size());
+    }
+
+    @Test
+    public void updateChangesHeroContent() throws Exception {
+        String initialName = "SubZero";
+        Hero hero = setupNewHero();
+        heroDao.add(hero);
+
+        heroDao.update(hero.getId(),"SubZero", 1);
+        Hero updatedTask = heroDao.findById(hero.getId());
+        assertNotEquals(initialName, updatedTask.getName());
+    }
+
+    @Test
+    public void deleteByIdDeletesCorrectHero() throws Exception {
+        Hero hero = setupNewHero();
+        heroDao.add(hero);
+        heroDao.deleteById(hero.getId());
+        assertEquals(0, heroDao.getAll().size());
+    }
+
+    @Test
+    public void clearAllClearsAll() throws Exception {
+        Hero hero = setupNewHero();
+        Hero otherHero = new Hero("SubZero", 2);
+        heroDao.add(hero);
+        heroDao.add(otherHero);
+        int daoSize = heroDao.getAll().size();
+        heroDao.clearAllHeroes();
+        assertTrue(daoSize > 0 && daoSize > heroDao.getAll().size());
+    }
+
+    @Test
+    public void squadIdIsReturnedCorrectly() throws Exception {
+        Hero hero = setupNewHero();
+        int originalSquadId = hero.getSquadId();
+        heroDao.add(hero);
+        assertEquals(originalSquadId, heroDao.findById(hero.getId()).getSquadId());
+    }
+
+    //define the following once and then call it as above in your tests.
+    public Hero setupNewHero(){
+        return new Hero("SubZero", 1);
     }
 }
