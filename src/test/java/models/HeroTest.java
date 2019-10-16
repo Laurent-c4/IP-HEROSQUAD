5488e74d3c4f39847c66bce1dc740db8d5784ac6
Laurent Juma <lcbillax.lj@gmail.com>
2019-10-16 09:56:57 +0300

    Set up tests for Hero Class

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,7 +1,33 @@
 package models;
 
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
 import static org.junit.Assert.*;
 
-public class HeroTest {
+public class TaskTest {
+    @Test
+    public void NewHeroObjectGetsCorrectlyCreated_true() throws Exception {
+        Hero hero = setupNewHero();
+        assertEquals(true, hero instanceof Hero);
+    }
 
+    @Test
+    public void TaskInstantiatesWithName_true() throws Exception {
+        Hero hero = setupNewHero();
+        assertEquals("SubZero", hero.getName());
+    }
+
+
+    @Test
+    public void getCreatedAtInstantiatesWithCurrentTimeToday() throws Exception {
+        Hero hero = setupNewHero();
+        assertEquals(LocalDateTime.now().getDayOfWeek(), task.getCreatedAt().getDayOfWeek());
+    }
+
+    //helper methods
+    public Hero setupNewHero(){
+        return new Hero("SubZero", 1);
+    }
 }
