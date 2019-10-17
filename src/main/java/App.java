350f6fea0ecf108ec5be0736006c68ba642b8a5c
Laurent Juma <lcbillax.lj@gmail.com>
2019-10-17 07:57:45 +0300

    Heroku Changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,6 +15,20 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        // This tells our app that if Heroku sets a port for us, we need to use that port.
+        // Otherwise, if they do not, continue using port 4567.
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+
         staticFileLocation("/public");
         String connectionString = "jdbc:postgresql://localhost:5432/herosquad";
         Sql2o sql2o = new Sql2o(connectionString, "laurent", "laurent");
